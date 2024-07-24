9936ac326a2ed295b4a7a32b49d82be76416440b
Fine <fine_567@hotmail.com>
2024-07-24 09:42:52 -0600

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { LocalStorageService } from './core/services/localstorage.service';
+import { TranslateService } from './core/services/translate.service';
+
+import { TranslatePipe } from './shared/pipes/translate.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TranslatePipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'website';
+  public translateService = inject(TranslateService); 
+  public localStorageService = inject(LocalStorageService); 
 }
